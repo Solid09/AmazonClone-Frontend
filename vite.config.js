bd91ee90164e5b09dc7027dd7f11a2d38a30f010
Solid09 <syedminhal03@gmail.com>
2025-02-06 20:24:27 +0500

    final v 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from 'fs';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -12,10 +10,6 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
-    },
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, './localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, './localhost.pem')),
     },
   },
 });
