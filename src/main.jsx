6192e1cee082331ece4df1da06be58599f95c6cb
Solid09 <syedminhal03@gmail.com>
2025-04-09 16:47:19 +0500

    connection to backend

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from './Redux/store.js';
+import axios from "axios";
+
+axios.defaults.baseURL = "https://amazonclone-backend-1b2978a6efa7.herokuapp.com/";
+// axios.defaults.baseURL = "http://localhost:5000/";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
