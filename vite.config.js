2a823ebfa09d9e21b45df9312d365e53c66d4573
Solid09 <syedminhal03@gmail.com>
2025-04-09 16:35:11 +0500

    connection to backend

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://localhost:3000",
+        target: "https://amazonclone-backend-1b2978a6efa7.herokuapp.com/",
         changeOrigin: true,
         secure: false,
       },
